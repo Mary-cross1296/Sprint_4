4d05cde59db0c03eff1e33eeeca3ddc13f7dd533
Maria Petrova <maria-petrova1296@yandex.ru>
2023-05-29 07:32:55 +0300

    Отчёт Allure

--- tests/test_order_page.py
+++ tests/test_order_page.py
@@ -1,5 +1,7 @@
+import allure
 class TestOrderPage:
 
+    @allure.title('Проверка перехода по логотипу "Самокат"')
     def test_logo_scoooter(self, driver, order_page):
         driver.get('https://qa-scooter.praktikum-services.ru/')
         order_page.wait_button_order_up()
@@ -7,6 +9,7 @@ class TestOrderPage:
         order_page.click_logo_scooter()
         assert order_page.get_page_url() == 'https://qa-scooter.praktikum-services.ru/'
 
+    @allure.title('Проверка перехода по кнопке "Заказать" вверху сраницы')
     def test_button_order_up(self, driver, order_page):
         driver.get('https://qa-scooter.praktikum-services.ru/')
         order_page.wait_button_order_up()
@@ -14,6 +17,7 @@ class TestOrderPage:
         order_page.wait_title_order_page()
         assert order_page.get_page_url() == 'https://qa-scooter.praktikum-services.ru/order'
 
+    @allure.title('Проверка перехода по кнопке "Заказать" в середине страницы')
     def test_button_order_middle(self, driver, order_page):
         driver.get('https://qa-scooter.praktikum-services.ru/')
         order_page.scroll_end_main_page()
@@ -22,6 +26,7 @@ class TestOrderPage:
         order_page.wait_title_order_page()
         assert order_page.get_page_url() == 'https://qa-scooter.praktikum-services.ru/order'
 
+    @allure.title('Проверка оформления заказа (с комментараем для курьера цвет самоката - черный)')
     def test_make_order_with_comment_and_color1(self, driver, order_page, new_user):
         driver.get('https://qa-scooter.praktikum-services.ru/')
         order_page.wait_button_order_up()
@@ -40,10 +45,11 @@ class TestOrderPage:
         order_page.click_make_order()
         assert order_page.confirmation_order() == "Отменить заказ"
 
+    @allure.title('Проверка оформления заказа (без комментария для курьера цвет самоката - серый)')
     def test_make_order_no_comment_and_color2(self, driver, order_page, new_user):
         driver.get('https://qa-scooter.praktikum-services.ru/')
-        order_page.wait_button_order_up()
-        order_page.click_button_order_up()
+        order_page.scroll_end_main_page()
+        order_page.click_button_order_middle()
         order_page.wait_title_order_page()
         order_page.enter_name(new_user.name)
         order_page.enter_last_name(new_user.last_name)
